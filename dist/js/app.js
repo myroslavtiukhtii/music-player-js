d8320a3009fe10099385a0e8bacd4cf441da7613
Myroslav <myroslavtp@gmail.com>
2023-02-07 21:47:02 +0200

    dev

--- dist/js/app.js
+++ dist/js/app.js
@@ -212,6 +212,15 @@
         COVERIMG.classList.add("active");
         script_menuOpen();
     }));
+    document.addEventListener("mousemove", parallax);
+    function parallax(event) {
+        this.querySelectorAll(".mouse").forEach((shift => {
+            const position = shift.getAttribute("value");
+            const x = (window.innerWidth - event.pageX * position) / 90;
+            const y = (window.innerHeight - event.pageY * position) / 90;
+            shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        }));
+    }
     window["FLS"] = true;
     isWebp();
 })();
